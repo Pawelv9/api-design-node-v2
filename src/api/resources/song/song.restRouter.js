c5f75ad0fc6375ef383f188035fda8019868c0dc
Pavel <Pawelv9@gmail.com>
2018-11-25 01:27:44 +0300

    lesson2

--- src/api/resources/song/song.restRouter.js
+++ src/api/resources/song/song.restRouter.js
@@ -2,3 +2,16 @@ import express from 'express'
 import songController from './song.controller'
 
 export const songRouter = express.Router()
+
+songRouter.param("id", songController.findByParam);
+
+userRouter
+  .route("/")
+  .get(songController.getAll)
+    .post(songController.createOne);
+
+userRouter
+  .route("/:id")
+  .get(songController.getOne)
+    .put(songController.updateOne)
+    .delete(songController.deleteOne);
--- src/server.js
+++ src/server.js
@@ -3,18 +3,22 @@ import setupMiddware from './middleware'
 import { restRouter } from './api'
 import { connect } from './db'
 import { signin, protect } from './api/modules/auth'
+
+
 // Declare an app from express
 const app = express()
 
 setupMiddware(app)
 connect()
-// setup basic routing for index route
 
 app.use('/signin', signin)
 
+app.use('/api', apiRouter)
+
 // catch all
+app.get('/', (req, res) => res.json({first: true}))
 app.all('*', (req, res) => {
-  res.json({ok: true})
+  res.json({ok: false})
 })
 
 export default app
